04dffccb44504a60e2f797161481e2fa790bc734
nzweb <nzweb@vk.com>
2023-01-02 21:09:25 +0300

    feat: add router

---
+++ services/frontend/src/features/counter/not-found/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+export const NotFoundPage = () => {
+    return (
+        <div>
+            404 Not Found
+            <Link to={'/'}>
+                To the Home page
+            </Link>
+        </div>
+    );
+}
--- services/frontend/src/main.tsx
+++ services/frontend/src/main.tsx
@@ -4,11 +4,24 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 import "./index.css";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {NotFoundPage} from "./features/counter/not-found/NotFoundPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "*",
+        element: <NotFoundPage  />
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
   </React.StrictMode>
 );
